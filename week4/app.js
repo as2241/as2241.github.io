c351f6d57cb30829777ac3c49259d5817ed77b05
Akbar Ali Salim Ali <as2241@hw.ac.uk>
2023-11-08 23:10:35 +0400

    Slightly change css code, Now stacking weather results.

--- week4/app.js
+++ week4/app.js
@@ -26,17 +26,23 @@ btn.addEventListener("click", function () {
   // When our response is received call the below function to run.
   ourRequest.onload = function () {
     // If our HTTP response status code is less than 200 or above 400 it indicates an HTTP status error.
-    if (ourRequest.status < 200 || ourRequest.status >= 400) {
-      // Display briefly something went wrong in weather-info division.
-      weatherContainer.innerHTML =
-        "Failed to fetch weather information. Please check console to debug further.";
-      // Display in console the error message.
-      console.error(`HTTP Error Status Code: ${ourRequest.status}`);
-    } else {
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
       // Once response is received and validated parse the JSON response and store it in 'data' variable to be further manipulated.
       const data = JSON.parse(ourRequest.responseText);
       renderHTML(data);
+    } else {
+      // Handle HTTP Status Code errors
+      handleErrors(new Error(`HTTP Error Status Code: ${ourRequest.status}`));
     }
+  };
+
+  // If there's an error during the network request, handle it.
+  ourRequest.onerror = function () {
+    handleErrors(
+      new Error(
+        "Network Error: Unable to complete the request. Please check your internet connection."
+      )
+    );
   };
 
   // Where the HTTP GET request is sent.
@@ -51,9 +57,19 @@ function renderHTML(data) {
   const cityName = data.name;
 
   // Append the variables to HTML paragraph tags and assign to 'htmlString'.
-  const htmlString = `<p>The weather in ${cityName} is ${weatherDescription}</p>
-                      <p>The temperature is ${cityMainTemp}°C with a wind speed of  ${windSpeed}m/s.</p>`;
+  const htmlString = `<p>The weather in ${cityName} is ${weatherDescription}.<br>
+                      The temperature is ${cityMainTemp}°C with a wind speed of ${windSpeed}m/s.</p>`;
 
   // Parse htmlString variable to be displayed inside weather-info division.
-  weatherContainer.insertAdjacentHTML("beforeend", htmlString);
+  weatherContainer.insertAdjacentHTML("afterbegin", htmlString);
+}
+
+function handleErrors(error) {
+  // Display briefly something went wrong in the weatherContainer
+  weatherContainer.insertAdjacentHTML(
+    "afterbegin",
+    `<p>Failed to fetch weather information. Please check console to debug further.</p>`
+  );
+  // Log the error to the console
+  console.error(error);
 }
